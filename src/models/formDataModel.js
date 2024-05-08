900b2d12a835782e5f11db706d21f4b569dc5a8c
digitalnzcheil <156040270+digitalnzcheil@users.noreply.github.com>
2024-05-09 00:55:46 +1200

    checking by reverting back the connection
    checking by reverting back the connection

--- src/models/formDataModel.js
+++ src/models/formDataModel.js
@@ -3,12 +3,9 @@ const mysql = require('mysql2');
 // Create a MySQL connection pool
 const pool = mysql.createPool({
     connectionLimit: 10,
-    host: process.env.AZURE_MYSQL_HOST,
-    user: process.env.AZURE_MYSQL_USER,
-    password: process.env.AZURE_MYSQL_PASSWORD,
-    database: process.env.AZURE_MYSQL_DATABASE,
-    port: process.env.AZURE_MYSQL_PORT,
-    ssl: process.env.AZURE_MYSQL_SSL ? JSON.parse(process.env.AZURE_MYSQL_SSL) : false // Parse boolean value if provided
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_DATABASE
 });
 
 // Function to execute SQL queries
