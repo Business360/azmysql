e26b74d5bcaedf900bcf307ced0d444530a7a60f
digitalnzcheil <156040270+digitalnzcheil@users.noreply.github.com>
2024-05-09 12:34:23 +1200

    Update formDataModel.js
    hidden database code

--- src/models/formDataModel.js
+++ src/models/formDataModel.js
@@ -7,43 +7,43 @@ console.log('Azure MySQL Database:', process.env.AZURE_MYSQL_DATABASE);
 console.log('Azure MySQL Port:', process.env.AZURE_MYSQL_PORT);
 console.log('Azure MySQL SSL:', process.env.AZURE_MYSQL_SSL);
 
-// Create a MySQL connection pool
-const pool = mysql.createPool({
-    connectionLimit: 10,
-    host: process.env.AZURE_MYSQL_HOST,
-    user: process.env.AZURE_MYSQL_USER,
-    password: process.env.AZURE_MYSQL_PASSWORD,
-    database: process.env.AZURE_MYSQL_DATABASE,
-    port: process.env.AZURE_MYSQL_PORT,
-    ssl: process.env.AZURE_MYSQL_SSL ? JSON.parse(process.env.AZURE_MYSQL_SSL) : false // Parse boolean value if provided
-});
+// // Create a MySQL connection pool
+// const pool = mysql.createPool({
+//     connectionLimit: 10,
+//     host: process.env.AZURE_MYSQL_HOST,
+//     user: process.env.AZURE_MYSQL_USER,
+//     password: process.env.AZURE_MYSQL_PASSWORD,
+//     database: process.env.AZURE_MYSQL_DATABASE,
+//     port: process.env.AZURE_MYSQL_PORT,
+//     ssl: process.env.AZURE_MYSQL_SSL ? JSON.parse(process.env.AZURE_MYSQL_SSL) : false // Parse boolean value if provided
+// });
 
-// Function to execute SQL queries
-const query = (sql, values) => {
-    // console.log(values);
-    return new Promise((resolve, reject) => {
-        pool.getConnection((err, connection) => {
-            if (err) {
-                return reject(err);
-            }
-            connection.query(sql, values, (err, results) => {
-                connection.release(); // release the connection
-                if (err) {
-                    return reject(err);
-                }
-                resolve(results);
-            });
-        });
-    });
-};
+// // Function to execute SQL queries
+// const query = (sql, values) => {
+//     // console.log(values);
+//     return new Promise((resolve, reject) => {
+//         pool.getConnection((err, connection) => {
+//             if (err) {
+//                 return reject(err);
+//             }
+//             connection.query(sql, values, (err, results) => {
+//                 connection.release(); // release the connection
+//                 if (err) {
+//                     return reject(err);
+//                 }
+//                 resolve(results);
+//             });
+//         });
+//     });
+// };
 
-// Function to check if a table exists in the database
-const tableExists = async (tableName) => {
-    const sql = `SELECT COUNT(*) AS count FROM information_schema.tables WHERE table_schema = ? AND table_name = ?`;
-    const values = [process.env.AZURE_MYSQL_DATABASE, tableName];
-    const result = await query(sql, values);
-    return result[0].count === 1;
-};
+// // Function to check if a table exists in the database
+// const tableExists = async (tableName) => {
+//     const sql = `SELECT COUNT(*) AS count FROM information_schema.tables WHERE table_schema = ? AND table_name = ?`;
+//     const values = [process.env.AZURE_MYSQL_DATABASE, tableName];
+//     const result = await query(sql, values);
+//     return result[0].count === 1;
+// };
 
 // Create a new form data entry
 exports.create = async ({
