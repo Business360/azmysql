65fa390486405acea6a1b6b0adfc78c377ea2ec6
digitalnzcheil <156040270+digitalnzcheil@users.noreply.github.com>
2024-05-09 14:59:44 +1200

    Update formDataModel.js
    connection string back to initial

--- src/models/formDataModel.js
+++ src/models/formDataModel.js
@@ -3,12 +3,9 @@ const mysql = require('mysql2');
 // Create a MySQL connection pool
 const pool = mysql.createPool({
     connectionLimit: 10,
-    host: process.env.AZURE_MYSQL_HOST,
-    user: process.env.AZURE_MYSQL_USER,
-    password: process.env.AZURE_MYSQL_PASSWORD,
-    database: process.env.AZURE_MYSQL_DATABASE,
-    port: process.env.AZURE_MYSQL_PORT,
-    // ssl: process.env.AZURE_MYSQL_SSL ? JSON.parse(process.env.AZURE_MYSQL_SSL) : false // Parse boolean value if provided
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_DATABASE
 });
 
 // Function to execute SQL queries
@@ -33,7 +30,7 @@ const query = (sql, values) => {
 // Function to check if a table exists in the database
 const tableExists = async (tableName) => {
     const sql = `SELECT COUNT(*) AS count FROM information_schema.tables WHERE table_schema = ? AND table_name = ?`;
-    const values = [process.env.AZURE_MYSQL_DATABASE, tableName];
+    const values = [process.env.DB_DATABASE, tableName];
     const result = await query(sql, values);
     return result[0].count === 1;
 };
