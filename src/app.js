3c8373cda7dedcb9235b7b1fa4558a19ca7df4af
digitalnzcheil <156040270+digitalnzcheil@users.noreply.github.com>
2024-05-09 13:24:25 +1200

    Add files via upload
    2nd initial commit

---
+++ src/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+// Create Express app
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
+
+// Routes
+const indexRouter = require('./routes/index');
+app.use('/', indexRouter);
+
+// Start the server
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
